2ffc3214cc85e670a35cd68f1cffbb8fbdba1a1a
ooscarr <loopainc>
2019-05-11 13:06:37 -0400

    No falla al rotar pantalla

--- app/src/main/java/cl/loopa/android/oldays/OldaysMapFragment.kt
+++ app/src/main/java/cl/loopa/android/oldays/OldaysMapFragment.kt
@@ -1,6 +1,8 @@
 package cl.loopa.android.oldays
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
@@ -11,64 +13,70 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.hardware.SensorManager
+import android.location.Criteria
+import android.location.LocationManager
 import android.net.Network
-import android.os.StrictMode
 import android.util.Log
 import androidx.core.content.ContextCompat.getSystemService
 
 import android.net.NetworkInfo
+import android.text.Html
 import android.widget.Toast
 import com.google.android.gms.maps.*
 
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-
-
-import org.osmdroid.bonuspack.kml.KmlDocument
 import org.osmdroid.bonuspack.kml.KmlFeature
 import com.google.android.gms.maps.MapsInitializer
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.Marker
-import org.json.JSONObject
+import com.google.android.gms.maps.model.*
 import org.osmdroid.bonuspack.kml.KmlFolder
 import org.osmdroid.bonuspack.kml.KmlPlacemark
 
-
 //import android.R
-
-
 
 
 class OldaysMapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
-    companion object {
+    var capas = ArrayList<KmlFeature>()
+
+    /*companion object {
         fun newInstance() = OldaysMapFragment()
-    }
+    }*/
 
     private lateinit var viewModel: OldaysMapViewModel
 
+
+    /* onCreateView is called to inflate the layout of the fragment
+      i.e graphical initialization usually takes place here.
+      It is always called sometimes after the onCreate method.
+       */
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
 
     ): View? {
-/*
+
         try {
             MapsInitializer.initialize(activity!!.applicationContext)
         } catch (e: Exception) {
             e.printStackTrace()
-        }*/
+        }
 
+
+        /* PONE EL MAPA */
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         // Androix dropped support libraries... support
-        //
+        // @ssantos https://stackoverflow.com/a/18963246/3369131
+        // MapFragment requires the native API Level 11 @CommonsWare https://stackoverflow.com/a/15407738/3369131
         var mapFragment : SupportMapFragment = SupportMapFragment.newInstance()
-        //mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        childFragmentManager.beginTransaction().add(R.id.map, mapFragment, "tag").commit()
+        //mapFragment = childFragmentManager!!.findFragmentById(R.id.map) as SupportMapFragment
+        childFragmentManager.beginTransaction().replace(R.id.map, mapFragment).commit()
         mapFragment.getMapAsync(this)
+
+        //getMap().setInfoWindowAdapter(PopupPinOldaysMapAdapter(inflater))
+
 
         return inflater.inflate(R.layout.fragment_oldays_map, container, false)
 
@@ -77,7 +85,21 @@ class OldaysMapFragment : Fragment(), OnMapReadyCallback {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(OldaysMapViewModel::class.java)
-        // TODO: Use the ViewModel
+
+
+        // TODO: Do it in a new thread and wait for results
+        // http://www.sgoliver.net/blog/tareas-en-segundo-plano-en-android-i-thread-y-asynctask/
+        //Thread(Runnable {
+            //Aquí ejecutamos nuestras tareas costosas
+
+            if (estaConectado()) {
+                Log.d("Conectado", "SÍ")
+
+                capas = viewModel.cargaKML()
+
+            }
+
+        //}).start()
 
     }
 
@@ -85,14 +107,21 @@ class OldaysMapFragment : Fragment(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        Log.d("Mapa", "Listo")
+
         // Add a marker in Sydney and move the camera
-        val iquique = LatLng(-20.2304908,-70.1398506)
-        mMap.addMarker(MarkerOptions().position(iquique).title("Iquique glorioso"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(iquique,13f))
+        val iquique = LatLng(-20.215120, -70.152103)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(iquique,15f))
 
         //mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE)
+        mMap.uiSettings.isZoomControlsEnabled = true
+        //mMap.isMyLocationEnabled = true
 
-        cargaKML()
+        ponePins(capas)
+
+        //https://pastebin.com/6UNWrrW2
+        //getMap().setOnInfoWindowClickListener(this)
+
     }
 
     // https://github.com/googlemaps/android-maps-utils/blob/master/demo/src/com/google/maps/android/utils/demo/BaseDemoActivity.java
@@ -101,77 +130,45 @@ class OldaysMapFragment : Fragment(), OnMapReadyCallback {
     }
 
 
-    fun cargaKML() {
+    fun ponePins(capas : ArrayList<KmlFeature>){
 
-        if (estaConectado()) {
-            Log.d("Conectado", "SÍ")
 
-            val urlString = "http://www.google.com/maps/d/kml?forcekml=1&mid=1X17DMfvcAVNH0qbrX-jJqSDDoAsHQYf6"
+        for (capa in capas) {
 
-            val kmlDocument = KmlDocument()
+            Log.d("kML", "Capa: " + capa.mName)
 
-            val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-            StrictMode.setThreadPolicy(policy)
+            val lugares = capa as KmlFolder
 
-            var ok: Boolean
-            try {
+            //if (lugares.mItems.size > 0) {
+            Log.d("kML", "Tiene " + lugares.mItems.size + " lugares ")
 
-                ok = kmlDocument.parseKMLUrl(urlString)
+            for (lugar: KmlFeature in lugares.mItems) {
 
-            } catch (e: Exception) {
-                e.printStackTrace()
-                ok = false
+                val punto = lugar as KmlPlacemark
+
+                Log.d("kML", "Punto: " + punto.mName)
+
+                val marker: Marker = getMap().addMarker(
+                    MarkerOptions()
+                        .position(
+                            LatLng(
+                                punto.mGeometry.boundingBox.centerLatitude,
+                                punto.mGeometry.boundingBox.centerLongitude
+                            )
+                        )
+                        .title(punto.mName)
+                        .snippet(stripHtml(punto.mDescription))
+                )
             }
 
-            if (ok) {
-                Log.d("Descargado", "OK")
-
-                if (kmlDocument.mKmlRoot != null) {
-                    //if (kmlDocument.mKmlRoot.mItems.size > 0) {
-
-                        for (capa in kmlDocument.mKmlRoot.mItems) {
-
-                            Log.d("kML", "Capa: " + capa.mName)
-
-                            val lugares = capa as KmlFolder
-
-                            //if (lugares.mItems.size > 0) {
-                                Log.d("kML", "Tiene " + lugares.mItems.size + " lugares ")
-
-                                for (lug: KmlFeature in lugares.mItems) {
-
-                                    val punto = lug as KmlPlacemark
-
-                                    Log.d("kML", "Punto: "+punto.mName)
-
-                                    val marker: Marker = getMap().addMarker(
-                                        MarkerOptions()
-                                            .position(LatLng(punto.mGeometry.boundingBox.centerLatitude, punto.mGeometry.boundingBox.centerLongitude))
-                                            .title(punto.mName)
-                                            .snippet(punto.getExtendedData("description")))
-
-                                }
-
-                            //}
-                        }
-
-                    //}
-                }
-
-
-            } else{
-                Toast.makeText(activity!!.applicationContext, "Error when loading KML", Toast.LENGTH_SHORT).show();
             }
 
-
-        } else {
-            Log.d("Conectado", "NO")
-        }
-
+        //}
     }
 
 
     /* DETECTAR SI HAY CONEXION */
+    // TODO: Move it to a class
     private fun estaConectado(): Boolean {
 
         // from fragment Context needs getActivity()?.
@@ -182,11 +179,10 @@ class OldaysMapFragment : Fragment(), OnMapReadyCallback {
         val tipoConexion = getConnectionType(activity!!.applicationContext)
 
         if(tipoConexion == 0){
-            Log.d("Conectado", "NO")
-
+            //Log.d("Conectado", "NO")
             return false
         }
-        Log.d("Conectado", "SÍ")
+        //Log.d("Conectado", "SÍ")
         return true
     }
 
@@ -217,6 +213,14 @@ class OldaysMapFragment : Fragment(), OnMapReadyCallback {
             }
         }
         return result
+    }
+
+    fun stripHtml(html: String): String {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+           return Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY).toString()
+        } else {
+           return Html.fromHtml(html).toString()
+        }
     }
 
 }
--- app/src/main/java/cl/loopa/android/oldays/OldaysMapViewModel.kt
+++ app/src/main/java/cl/loopa/android/oldays/OldaysMapViewModel.kt
@@ -1,9 +1,56 @@
 package cl.loopa.android.oldays
 
+import android.os.StrictMode
+import android.util.Log
 import androidx.lifecycle.ViewModel;
+import org.osmdroid.bonuspack.kml.KmlDocument
+import org.osmdroid.bonuspack.kml.KmlFeature
 
 class OldaysMapViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+
+
+
+    fun cargaKML() : ArrayList<KmlFeature>{
+
+
+            val urlString = "http://www.google.com/maps/d/kml?forcekml=1&mid=1X17DMfvcAVNH0qbrX-jJqSDDoAsHQYf6"
+
+            val kmlDocument = KmlDocument()
+
+            val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+
+            var ok: Boolean
+            try {
+
+                ok = kmlDocument.parseKMLUrl(urlString)
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+                ok = false
+            }
+
+            if (ok) {
+                Log.d("Descargado", "OK")
+
+                if (kmlDocument.mKmlRoot != null) {
+                    //if (kmlDocument.mKmlRoot.mItems.size > 0) {
+
+                    return kmlDocument.mKmlRoot.mItems
+
+                    //}
+                }
+
+            } else{
+                // Toast.makeText(activity!!.applicationContext, "Error when loading KML", Toast.LENGTH_SHORT).show();
+
+            }
+
+        return ArrayList<KmlFeature>()
+    }
+
+
 
 
 }
